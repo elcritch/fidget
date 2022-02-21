08ed1b68097d7f167edbd61651ed0bc7f8205dec
Jaremy Creechley <creechley@gmail.com>
2022-02-21 02:00:25 -0800

    prettying up things

--- tests/barsscroll/barsscroll.nim
+++ tests/barsscroll/barsscroll.nim
@@ -40,12 +40,11 @@ proc drawMain() =
           rectangle "dec":
             box 0, 0, 40, 40
             fill "#AEB5C0"
-            cornerRadius 5
+            cornerRadius 3
             onHover:
               fill "#46DE5F"
             onClick:
               bar -= 0.05
-              bar = bar.clamp(0.0, 1.0)
             instance "arrow":
               box 0, 0, 40, 40
               rotation -180
@@ -59,24 +58,25 @@ proc drawMain() =
           rectangle "inc":
             box barW-80, 0, 40, 40
             fill "#AEB5C0"
-            cornerRadius 5
+            cornerRadius 3
             onHover:
               fill "#46DE5F"
             onClick:
               bar += 0.05
-              bar = bar.clamp(0.0, 1.0)
             instance "arrow":
               box 0, 0, 40, 40
               image "arrow.png"
+
+          bar = bar.clamp(0.001, 1.0)
 
           # Draw the bar itself.
           group "bar":
             box 60, 0, barW - 80*2, 40
             fill "#F7F7F9"
+            cornerRadius 5
             rectangle "barFg":
               box 0, 0, (barW - 80*2) * float(bar), 40
               fill "#46D15F"
-          
-          bar = bar.clamp(0.0, 1.0)
+              cornerRadius 5
 
 startFidget(drawMain, uiScale=1.5)
