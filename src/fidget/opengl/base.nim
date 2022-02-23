a75cbbe9db3b3c0e3688c16e00c73ce4d02ed4f3
Jaremy Creechley <creechley@gmail.com>
2022-02-23 01:55:13 -0800

    tweaking scrollbars

--- src/fidget/opengl/base.nim
+++ src/fidget/opengl/base.nim
@@ -193,9 +193,11 @@ proc glGetInteger*(what: GLenum): int =
 
 proc onResize(handle: staticglfw.Window, w, h: int32) {.cdecl.} =
   updateWindowSize()
+  let prevloopMode = loopMode
   updateLoop(poll = false)
   loopMode = RepaintOnFrame
   updateLoop(poll = false)
+  loopMode = prevloopMode
 
 proc onFocus(window: staticglfw.Window, state: cint) {.cdecl.} =
   focused = state == 1
