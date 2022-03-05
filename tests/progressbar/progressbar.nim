8ad44a0c408ed729b8a40da93ef344aae10a1967
Jaremy Creechley <creechley@gmail.com>
2022-03-05 01:58:17 -0800

    progress bar setup

--- tests/progressbar/progressbar.nim
+++ tests/progressbar/progressbar.nim
@@ -3,16 +3,14 @@ import bumpy, fidget, math, random
 loadFont("IBM Plex Sans", "IBMPlexSans-Regular.ttf")
 
 # Create an array of 30 bars.
-var bars = newSeq[float](1)
-for i, bar in bars:
-  bars[i] = rand(1.0)
+var bar: float = 0.5
 
 proc drawMain() =
   # Set the window title.
   setTitle("Fidget Bars Example")
 
   # Use simple math to layout things.
-  let barH = bars.len.float32 * 60 + 20
+  let barH = 60 + 20
   let barW = root.box().w - 100
 
   group "button":
@@ -46,56 +44,55 @@ proc drawMain() =
       scrollBars true
 
       # Draw a list of bars using a simple for loop.
-      for i, bar in bars.mpairs:
-        group "bar":
-          box 20, 20 + 60 * i, barW, 60
+      group "bar":
+        box 20, 20 + 60, barW, 60
 
-          text "text":
-            box 61, 0, 70, 20
-            fill "#46607e"
-            characters "scroll " & $i
+        text "text":
+          box 61, 0, 70, 20
+          fill "#46607e"
+          characters "progress"
 
-          # Draw the decrement button to make the bar go down.
-          rectangle "dec":
+        # Draw the decrement button to make the bar go down.
+        rectangle "dec":
+          box 0, 0, 40, 40
+          fill "#AEB5C0"
+          cornerRadius 3
+          onHover:
+            fill "#46DE5F"
+          onClick:
+            bar -= 0.05
+          instance "arrow":
             box 0, 0, 40, 40
-            fill "#AEB5C0"
-            cornerRadius 3
-            onHover:
-              fill "#46DE5F"
-            onClick:
-              bar -= 0.05
-            instance "arrow":
-              box 0, 0, 40, 40
-              rotation -180
-              image "arrow.png"
+            rotation -180
+            image "arrow.png"
 
-          # Draw the increment button to make the bar go up.
-          rectangle "inc":
-            box barW-80, 0, 40, 40
-            fill "#AEB5C0"
-            cornerRadius 3
-            onHover:
-              fill "#46DE5F"
-            onClick:
-              bar += 0.05
-            instance "arrow":
-              box 0, 0, 40, 40
-              image "arrow.png"
+        # Draw the increment button to make the bar go up.
+        rectangle "inc":
+          box barW-80, 0, 40, 40
+          fill "#AEB5C0"
+          cornerRadius 3
+          onHover:
+            fill "#46DE5F"
+          onClick:
+            bar += 0.05
+          instance "arrow":
+            box 0, 0, 40, 40
+            image "arrow.png"
 
-          bar = bar.clamp(0.001, 1.0)
+        bar = bar.clamp(0.001, 1.0)
 
-          # Draw the bar itself.
-          group "bar":
-            box 60, 0, barW - 80*2, 40
-            fill "#F7F7F9"
+        # Draw the bar itself.
+        group "bar":
+          box 60, 0, barW - 80*2, 40
+          fill "#F7F7F9"
+          cornerRadius 5
+          rectangle "barFg":
+            box 0, 0, (barW - 80*2) * float(bar), 40
+            fill "#46D15F"
             cornerRadius 5
-            rectangle "barFg":
-              box 0, 0, (barW - 80*2) * float(bar), 40
-              fill "#46D15F"
-              cornerRadius 5
-            onScroll:
-              # echo "scrolled: ", mouse.wheelDelta
-              bar += mouse.wheelDelta * 1.0e-3
+          onScroll:
+            # echo "scrolled: ", mouse.wheelDelta
+            bar += mouse.wheelDelta * 1.0e-3
 
 
 startFidget(drawMain, uiScale=1.5)
