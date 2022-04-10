69e8f5b19fce2e584fbb6059071ecbbb80cca78c
Jaremy Creechley <creechley@gmail.com>
2022-04-09 22:18:11 -0700

    refactor draw proc

---
+++ src/fidget/opengl/draw.nim
@@ -0,0 +1,330 @@
+import chroma, hashes, 
+    os, strformat, strutils, tables, times, typography,
+    typography/textboxes, unicode, vmath, bumpy,
+    typography/svgfont, pixie
+
+import base, context, formatflippy
+import ../input, ../internal, base
+import ../common
+
+type
+  Context = context.Context
+
+var
+  ctx*: Context
+  glyphOffsets: Table[Hash, Vec2]
+
+  # Used for double-clicking
+  multiClick: int
+  lastClickTime: float
+  currLevel: ZLevel
+
+proc refresh*() =
+  ## Request the screen be redrawn
+  requestedFrame = true
+
+proc focus*(keyboard: Keyboard, node: Node) =
+  if keyboard.focusNode != node:
+    keyboard.onUnFocusNode = keyboard.focusNode
+    keyboard.onFocusNode = node
+    keyboard.focusNode = node
+
+    var font = fonts[node.textStyle.fontFamily]
+    font.size = node.textStyle.fontSize
+    font.lineHeight = node.textStyle.lineHeight
+    # if font.lineHeight == 0:
+      # font.lineHeight = font.size
+    keyboard.input = node.text
+    textBox = newTextBox(
+      font,
+      int node.screenBox.w,
+      int node.screenBox.h,
+      node.text,
+      hAlignMode(node.textStyle.textAlignHorizontal),
+      vAlignMode(node.textStyle.textAlignVertical),
+      node.multiline,
+      worldWrap = true,
+    )
+    textBox.editable = node.editableText
+    textBox.scrollable = true
+
+    refresh()
+
+proc unFocus*(keyboard: Keyboard, node: Node) =
+  if keyboard.focusNode == node:
+    keyboard.onUnFocusNode = keyboard.focusNode
+    keyboard.onFocusNode = nil
+    keyboard.focusNode = nil
+
+
+proc drawText(node: Node) =
+  if node.textStyle.fontFamily notin fonts:
+    quit &"font not found: {node.textStyle.fontFamily}"
+
+  var font = fonts[node.textStyle.fontFamily]
+  font.size = node.textStyle.fontSize
+  font.lineHeight = node.textStyle.lineHeight
+  if font.lineHeight == 0:
+    font.lineHeight = font.size
+
+  let mousePos = mouse.pos - node.screenBox.xy
+
+  if mouse.pos.overlaps(node.screenBox):
+    if node.selectable and mouse.wheelDelta != 0:
+      keyboard.focus(node)
+    elif node.selectable and mouse.down:
+      # mouse actions click, drag, double clicking
+      keyboard.focus(node)
+      if mouse.click:
+        if epochTime() - lastClickTime < 0.5:
+          inc multiClick
+        else:
+          multiClick = 0
+        lastClickTime = epochTime()
+        if multiClick == 1:
+          textBox.selectWord(mousePos)
+          buttonDown[MOUSE_LEFT] = false
+        elif multiClick == 2:
+          textBox.selectParagraph(mousePos)
+          buttonDown[MOUSE_LEFT] = false
+        elif multiClick == 3:
+          textBox.selectAll()
+          buttonDown[MOUSE_LEFT] = false
+        else:
+          textBox.mouseAction(mousePos, click = true, keyboard.shiftKey)
+
+  if textBox != nil and
+      mouse.down and
+      not mouse.click and
+      keyboard.focusNode == node:
+    # Dragging the mouse:
+    textBox.mouseAction(mousePos, click = false, keyboard.shiftKey)
+
+  let editing = keyboard.focusNode == node
+
+  if editing:
+    if textBox.size != node.screenBox.wh:
+      textBox.resize(node.screenBox.wh)
+    node.textLayout = textBox.layout
+    ctx.saveTransform()
+    ctx.translate(-textBox.scroll)
+    for rect in textBox.selectionRegions():
+      ctx.fillRect(rect, node.highlightColor)
+  else:
+    discard
+
+  # draw characters
+  for glyphIdx, pos in node.textLayout:
+    if pos.character notin font.typeface.glyphs:
+      continue
+    if pos.rune == Rune(32):
+      # Don't draw space, even if font has a char for it.
+      continue
+
+    let
+      font = pos.font
+      subPixelShift = floor(pos.subPixelShift * 10) / 10
+      fontFamily = node.textStyle.fontFamily
+
+    var
+      hashFill = hash((
+        2344,
+        fontFamily,
+        pos.character,
+        (font.size*100).int,
+        (subPixelShift*100).int,
+        0
+      ))
+      hashStroke: Hash
+
+    if node.strokeWeight > 0:
+      hashStroke = hash((
+        9812,
+        fontFamily,
+        pos.character,
+        (font.size*100).int,
+        (subPixelShift*100).int,
+        node.strokeWeight
+      ))
+
+    if hashFill notin ctx.entries:
+      var
+        glyph = font.typeface.glyphs[pos.character]
+        glyphOffset: Vec2
+      let glyphFill = font.getGlyphImage(
+        glyph,
+        glyphOffset,
+        subPixelShift = subPixelShift
+      )
+      ctx.putImage(hashFill, glyphFill)
+      glyphOffsets[hashFill] = glyphOffset
+
+    if node.strokeWeight > 0 and hashStroke notin ctx.entries:
+      var
+        glyph = font.typeface.glyphs[pos.character]
+        glyphOffset: Vec2
+      let glyphFill = font.getGlyphImage(
+        glyph,
+        glyphOffset,
+        subPixelShift = subPixelShift
+      )
+      let glyphStroke = glyphFill.outlineBorder(node.strokeWeight.int)
+      ctx.putImage(hashStroke, glyphStroke)
+
+    let
+      glyphOffset = glyphOffsets[hashFill]
+      charPos = vec2(pos.rect.x + glyphOffset.x, pos.rect.y + glyphOffset.y)
+
+    if node.strokeWeight > 0 and node.stroke.a > 0:
+      ctx.drawImage(
+        hashStroke,
+        charPos - vec2(node.strokeWeight, node.strokeWeight),
+        node.stroke
+      )
+
+    ctx.drawImage(hashFill, charPos, node.fill)
+
+  if editing:
+    if textBox.cursor == textBox.selector and node.editableText:
+      # draw cursor
+      ctx.fillRect(textBox.cursorRect, node.cursorColor)
+    # debug
+    # ctx.fillRect(textBox.selectorRect, rgba(0, 0, 0, 255).color)
+    # ctx.fillRect(rect(textBox.mousePos, vec2(4, 4)), rgba(255, 128, 128, 255).color)
+    ctx.restoreTransform()
+
+
+proc removeExtraChildren*(node: Node) =
+  ## Deal with removed nodes.
+  node.nodes.setLen(node.diffIndex)
+
+import macros
+
+macro ifdraw(check, code: untyped, post: untyped = nil) =
+  ## check if code should be drawn
+  result = newStmtList()
+  let checkval = genSym(nskLet, "checkval")
+  result.add quote do:
+      let `checkval` = node.zLevel == currLevel and `check`
+      if `checkval`: `code`
+  if post != nil:
+    post.expectKind(nnkFinally)
+    let postBlock = post[0]
+    result.add quote do:
+      defer:
+        if `checkval`: `postBlock`
+
+proc drawMasks*(node: Node) =
+  if node.cornerRadius[0] != 0:
+    ctx.fillRoundedRect(rect(
+      0, 0,
+      node.screenBox.w, node.screenBox.h
+    ), rgba(255, 0, 0, 255).color, node.cornerRadius[0])
+  else:
+    ctx.fillRect(rect(
+      0, 0,
+      node.screenBox.w, node.screenBox.h
+    ), rgba(255, 0, 0, 255).color)
+
+proc drawShadows*(node: Node) =
+  let shadow = node.shadows[0]
+
+  let blur = shadow.blur / 7.0
+  for i in 0..6:
+    # for j in 0..4:
+    let j = i
+    ctx.fillRoundedRect(rect(
+      shadow.x + uiScale*i.toFloat()*blur, shadow.y + uiScale*j.toFloat()*blur,
+      node.screenBox.w, node.screenBox.h
+    ), shadow.color, node.cornerRadius[0])
+    
+proc drawBoxes*(node: Node) =
+  if node.fill.a > 0:
+    if node.imageName == "":
+      if node.cornerRadius[0] != 0:
+        ctx.fillRoundedRect(rect(
+          0, 0,
+          node.screenBox.w, node.screenBox.h
+        ), node.fill, node.cornerRadius[0])
+      else:
+        ctx.fillRect(rect(
+          0, 0,
+          node.screenBox.w, node.screenBox.h
+        ), node.fill)
+
+  if node.stroke.a > 0 and node.strokeWeight > 0 and node.kind != nkText:
+    ctx.strokeRoundedRect(rect(
+      0, 0,
+      node.screenBox.w, node.screenBox.h
+    ), node.stroke, node.strokeWeight, node.cornerRadius[0])
+
+  if node.imageName != "":
+    let path = dataDir / node.imageName
+    ctx.drawImage(path, pos = vec2(0, 0), color = node.imageColor, size = vec2(node.screenBox.w, node.screenBox.h))
+
+proc draw*(node, parent: Node) =
+  ## Draws the node.
+  ## 
+  ## This is the primary routine that handles setting up the OpenGL
+  ## context that will get rendered. This doesn't trigger the actual
+  ## OpenGL rendering, but configures the various shaders and elements.
+  ## 
+  ## Note that visiable draw calls need to check they're on the current
+  ## active ZLevel (z-index). 
+
+  # setup the opengl context to match the current node size and position
+  ctx.saveTransform()
+  ctx.translate(node.screenBox.xy)
+
+  # handles setting up scrollbar region
+  ifdraw node.id == "$scrollbar":
+    ctx.saveTransform()
+    ctx.translate(parent.offset)
+  finally:
+    ctx.restoreTransform()
+
+  # handles setting up scrollbar region
+  ifdraw node.rotation != 0:
+    ctx.translate(node.screenBox.wh/2)
+    ctx.rotate(node.rotation/180*PI)
+    ctx.translate(-node.screenBox.wh/2)
+
+  # handle clipping children content based on this node
+  ifdraw node.clipContent:
+    ctx.beginMask()
+    node.drawMasks()
+    ctx.endMask()
+  finally:
+    ctx.popMask()
+
+  # hacky method to draw drop shadows... should probably be done in opengl sharders
+  ifdraw node.shadows.len() > 0:
+    node.drawShadows()
+
+  ifdraw true:
+    # draw visiable decorations for node
+    if node.kind == nkText:
+      node.drawText()
+    else:
+      node.drawBoxes()
+    
+  # restores the opengl context back to the parent node's (see above)
+  ctx.restoreTransform()
+
+  ifdraw node.scrollBars:
+    # handles drawing actual scrollbars
+    ctx.saveTransform()
+    ctx.translate(-node.offset)
+  finally:
+    ctx.restoreTransform()
+
+  for j in 1 .. node.nodes.len:
+    node.nodes[^j].draw(node)
+
+  # finally blocks will be run here, in reverse order
+
+proc draw*(root: Node) =
+  for zidx in ZLevel:
+    # draw root for each level
+    currLevel = zidx
+    root.draw(root)
--- src/fidget/openglbackend.nim
+++ src/fidget/openglbackend.nim
@@ -3,25 +3,17 @@ import chroma, common, hashes, input, internal, opengl/base,
     typography/textboxes, unicode, vmath, opengl/formatflippy, bumpy,
     typography/svgfont, pixie
 
+import opengl/draw
+
 when not defined(emscripten) and not defined(fidgetNoAsync):
   import httpClient, asyncdispatch, asyncfutures, json
 
-export input
+export input, draw
 
 import sugar
 
-type
-  Context = context.Context
-
 var
-  ctx*: Context
-  glyphOffsets: Table[Hash, Vec2]
   windowTitle, windowUrl: string
-
-  # Used for double-clicking
-  multiClick: int
-  lastClickTime: float
-  currLevel: ZLevel
 
 computeTextLayout = proc(node: Node) =
   var font = fonts[node.textStyle.fontFamily]
@@ -59,309 +51,6 @@ proc processHooks(parent, node: Node) =
   for child in node.nodes:
     processHooks(node, child)
 
-proc refresh*() =
-  ## Request the screen be redrawn
-  requestedFrame = true
-
-proc focus*(keyboard: Keyboard, node: Node) =
-  if keyboard.focusNode != node:
-    keyboard.onUnFocusNode = keyboard.focusNode
-    keyboard.onFocusNode = node
-    keyboard.focusNode = node
-
-    var font = fonts[node.textStyle.fontFamily]
-    font.size = node.textStyle.fontSize
-    font.lineHeight = node.textStyle.lineHeight
-    # if font.lineHeight == 0:
-      # font.lineHeight = font.size
-    keyboard.input = node.text
-    textBox = newTextBox(
-      font,
-      int node.screenBox.w,
-      int node.screenBox.h,
-      node.text,
-      hAlignMode(node.textStyle.textAlignHorizontal),
-      vAlignMode(node.textStyle.textAlignVertical),
-      node.multiline,
-      worldWrap = true,
-    )
-    textBox.editable = node.editableText
-    textBox.scrollable = true
-
-    refresh()
-
-proc unFocus*(keyboard: Keyboard, node: Node) =
-  if keyboard.focusNode == node:
-    keyboard.onUnFocusNode = keyboard.focusNode
-    keyboard.onFocusNode = nil
-    keyboard.focusNode = nil
-
-proc drawText(node: Node) =
-  if node.textStyle.fontFamily notin fonts:
-    quit &"font not found: {node.textStyle.fontFamily}"
-
-  var font = fonts[node.textStyle.fontFamily]
-  font.size = node.textStyle.fontSize
-  font.lineHeight = node.textStyle.lineHeight
-  if font.lineHeight == 0:
-    font.lineHeight = font.size
-
-  let mousePos = mouse.pos - node.screenBox.xy
-
-  if mouse.pos.overlaps(node.screenBox):
-    if node.selectable and mouse.wheelDelta != 0:
-      keyboard.focus(node)
-    elif node.selectable and mouse.down:
-      # mouse actions click, drag, double clicking
-      keyboard.focus(node)
-      if mouse.click:
-        if epochTime() - lastClickTime < 0.5:
-          inc multiClick
-        else:
-          multiClick = 0
-        lastClickTime = epochTime()
-        if multiClick == 1:
-          textBox.selectWord(mousePos)
-          buttonDown[MOUSE_LEFT] = false
-        elif multiClick == 2:
-          textBox.selectParagraph(mousePos)
-          buttonDown[MOUSE_LEFT] = false
-        elif multiClick == 3:
-          textBox.selectAll()
-          buttonDown[MOUSE_LEFT] = false
-        else:
-          textBox.mouseAction(mousePos, click = true, keyboard.shiftKey)
-
-  if textBox != nil and
-      mouse.down and
-      not mouse.click and
-      keyboard.focusNode == node:
-    # Dragging the mouse:
-    textBox.mouseAction(mousePos, click = false, keyboard.shiftKey)
-
-  let editing = keyboard.focusNode == node
-
-  if editing:
-    if textBox.size != node.screenBox.wh:
-      textBox.resize(node.screenBox.wh)
-    node.textLayout = textBox.layout
-    ctx.saveTransform()
-    ctx.translate(-textBox.scroll)
-    for rect in textBox.selectionRegions():
-      ctx.fillRect(rect, node.highlightColor)
-  else:
-    discard
-
-  # draw characters
-  for glyphIdx, pos in node.textLayout:
-    if pos.character notin font.typeface.glyphs:
-      continue
-    if pos.rune == Rune(32):
-      # Don't draw space, even if font has a char for it.
-      continue
-
-    let
-      font = pos.font
-      subPixelShift = floor(pos.subPixelShift * 10) / 10
-      fontFamily = node.textStyle.fontFamily
-
-    var
-      hashFill = hash((
-        2344,
-        fontFamily,
-        pos.character,
-        (font.size*100).int,
-        (subPixelShift*100).int,
-        0
-      ))
-      hashStroke: Hash
-
-    if node.strokeWeight > 0:
-      hashStroke = hash((
-        9812,
-        fontFamily,
-        pos.character,
-        (font.size*100).int,
-        (subPixelShift*100).int,
-        node.strokeWeight
-      ))
-
-    if hashFill notin ctx.entries:
-      var
-        glyph = font.typeface.glyphs[pos.character]
-        glyphOffset: Vec2
-      let glyphFill = font.getGlyphImage(
-        glyph,
-        glyphOffset,
-        subPixelShift = subPixelShift
-      )
-      ctx.putImage(hashFill, glyphFill)
-      glyphOffsets[hashFill] = glyphOffset
-
-    if node.strokeWeight > 0 and hashStroke notin ctx.entries:
-      var
-        glyph = font.typeface.glyphs[pos.character]
-        glyphOffset: Vec2
-      let glyphFill = font.getGlyphImage(
-        glyph,
-        glyphOffset,
-        subPixelShift = subPixelShift
-      )
-      let glyphStroke = glyphFill.outlineBorder(node.strokeWeight.int)
-      ctx.putImage(hashStroke, glyphStroke)
-
-    let
-      glyphOffset = glyphOffsets[hashFill]
-      charPos = vec2(pos.rect.x + glyphOffset.x, pos.rect.y + glyphOffset.y)
-
-    if node.strokeWeight > 0 and node.stroke.a > 0:
-      ctx.drawImage(
-        hashStroke,
-        charPos - vec2(node.strokeWeight, node.strokeWeight),
-        node.stroke
-      )
-
-    ctx.drawImage(hashFill, charPos, node.fill)
-
-  if editing:
-    if textBox.cursor == textBox.selector and node.editableText:
-      # draw cursor
-      ctx.fillRect(textBox.cursorRect, node.cursorColor)
-    # debug
-    # ctx.fillRect(textBox.selectorRect, rgba(0, 0, 0, 255).color)
-    # ctx.fillRect(rect(textBox.mousePos, vec2(4, 4)), rgba(255, 128, 128, 255).color)
-    ctx.restoreTransform()
-
-
-proc removeExtraChildren*(node: Node) =
-  ## Deal with removed nodes.
-  node.nodes.setLen(node.diffIndex)
-
-import macros
-
-macro ifdraw(check, code: untyped, post: untyped = nil) =
-  ## check if code should be drawn
-  result = newStmtList()
-  let checkval = genSym(nskLet, "checkval")
-  result.add quote do:
-      let `checkval` = node.zLevel == currLevel and `check`
-      if `checkval`: `code`
-  if post != nil:
-    post.expectKind(nnkFinally)
-    let postBlock = post[0]
-    result.add quote do:
-      defer:
-        if `checkval`: `postBlock`
-
-proc drawMasks*(node: Node) =
-  if node.cornerRadius[0] != 0:
-    ctx.fillRoundedRect(rect(
-      0, 0,
-      node.screenBox.w, node.screenBox.h
-    ), rgba(255, 0, 0, 255).color, node.cornerRadius[0])
-  else:
-    ctx.fillRect(rect(
-      0, 0,
-      node.screenBox.w, node.screenBox.h
-    ), rgba(255, 0, 0, 255).color)
-
-proc drawShadows*(node: Node) =
-  let shadow = node.shadows[0]
-
-  let blur = shadow.blur / 7.0
-  for i in 0..6:
-    # for j in 0..4:
-    let j = i
-    ctx.fillRoundedRect(rect(
-      shadow.x + uiScale*i.toFloat()*blur, shadow.y + uiScale*j.toFloat()*blur,
-      node.screenBox.w, node.screenBox.h
-    ), shadow.color, node.cornerRadius[0])
-    
-proc drawBoxes*(node: Node) =
-  if node.fill.a > 0:
-    if node.imageName == "":
-      if node.cornerRadius[0] != 0:
-        ctx.fillRoundedRect(rect(
-          0, 0,
-          node.screenBox.w, node.screenBox.h
-        ), node.fill, node.cornerRadius[0])
-      else:
-        ctx.fillRect(rect(
-          0, 0,
-          node.screenBox.w, node.screenBox.h
-        ), node.fill)
-
-  if node.stroke.a > 0 and node.strokeWeight > 0 and node.kind != nkText:
-    ctx.strokeRoundedRect(rect(
-      0, 0,
-      node.screenBox.w, node.screenBox.h
-    ), node.stroke, node.strokeWeight, node.cornerRadius[0])
-
-  if node.imageName != "":
-    let path = dataDir / node.imageName
-    ctx.drawImage(path, pos = vec2(0, 0), color = node.imageColor, size = vec2(node.screenBox.w, node.screenBox.h))
-
-proc draw*(node, parent: Node) =
-  ## Draws the node.
-  ## 
-  ## This is the primary routine that handles setting up the OpenGL
-  ## context that will get rendered. This doesn't trigger the actual
-  ## OpenGL rendering, but configures the various shaders and elements.
-  ## 
-  ## Note that visiable draw calls need to check they're on the current
-  ## active ZLevel (z-index). 
-
-  # setup the opengl context to match the current node size and position
-  ctx.saveTransform()
-  ctx.translate(node.screenBox.xy)
-
-  # handles setting up scrollbar region
-  ifdraw node.id == "$scrollbar":
-    ctx.saveTransform()
-    ctx.translate(parent.offset)
-  finally:
-    ctx.restoreTransform()
-
-  # handles setting up scrollbar region
-  ifdraw node.rotation != 0:
-    ctx.translate(node.screenBox.wh/2)
-    ctx.rotate(node.rotation/180*PI)
-    ctx.translate(-node.screenBox.wh/2)
-
-  # handle clipping children content based on this node
-  ifdraw node.clipContent:
-    ctx.beginMask()
-    node.drawMasks()
-    ctx.endMask()
-  finally:
-    ctx.popMask()
-
-  # hacky method to draw drop shadows... should probably be done in opengl sharders
-  ifdraw node.shadows.len() > 0:
-    node.drawShadows()
-
-  ifdraw true:
-    # draw visiable decorations for node
-    if node.kind == nkText:
-      node.drawText()
-    else:
-      node.drawBoxes()
-    
-  # restores the opengl context back to the parent node's (see above)
-  ctx.restoreTransform()
-
-  ifdraw node.scrollBars:
-    # handles drawing actual scrollbars
-    ctx.saveTransform()
-    ctx.translate(-node.offset)
-  finally:
-    ctx.restoreTransform()
-
-  for j in 1 .. node.nodes.len:
-    node.nodes[^j].draw(node)
-
-  # finally blocks will be run here, in reverse order
-
 
 proc openBrowser*(url: string) =
   ## Opens a URL in a browser
@@ -373,12 +62,12 @@ proc openBrowser*(url: string) =
 #     echo "window: "
 
 proc setupFidget(
-  openglVersion: (int, int),
-  msaa: MSAA,
-  mainLoopMode: MainLoopMode,
-  pixelate: bool,
-  forcePixelScale: float32,
-  atlasSize: int = 1024
+    openglVersion: (int, int),
+    msaa: MSAA,
+    mainLoopMode: MainLoopMode,
+    pixelate: bool,
+    forcePixelScale: float32,
+    atlasSize: int = 1024
 ) =
   pixelScale = forcePixelScale
 
@@ -417,10 +106,7 @@ proc setupFidget(
     processHooks(nil, root)
 
     # Only draw the root after everything was done:
-    for zidx in ZLevel:
-      # draw root for each level
-      currLevel = zidx
-      root.draw(root)
+    root.draw()
 
     ctx.restoreTransform()
     ctx.endFrame()
@@ -458,40 +144,26 @@ proc asyncPoll() =
       if isEvent:
         isEvent = false
         eventTimePost = epochTime()
-        echo "user input event delay: ", eventTimePost - eventTimePre
-
-    # var haveCalls = false
-    # for call in httpCalls.values:
-    #   if call.status == Loading:
-    #     haveCalls = true
-    #     break
-    # if haveCalls:
-    #   poll()
-
-import os
-proc timerFunc() {.thread.} =
-  while true:
-    echo fmt"dtAvg: {dtAvg=} fps: {fps=} avgFrameTime: {avgFrameTime=}"
-    os.sleep(1_000)
-
+# 
 type
   MainProc* = proc () 
 
 proc startFidget*(
-  draw: proc() = nil,
-  tick: proc() = nil,
-  load: proc() = nil,
-  fullscreen = false,
-  w: Positive = 1280,
-  h: Positive = 800,
-  openglVersion = (3, 3),
-  msaa = msaaDisabled,
-  mainLoopMode: MainLoopMode = RepaintOnEvent,
-  pixelate = false,
-  pixelScale = 1.0,
-  uiScale = 1.0
+    draw: proc() = nil,
+    tick: proc() = nil,
+    load: proc() = nil,
+    fullscreen = false,
+    w: Positive = 1280,
+    h: Positive = 800,
+    openglVersion = (3, 3),
+    msaa = msaaDisabled,
+    mainLoopMode: MainLoopMode = RepaintOnEvent,
+    pixelate = false,
+    pixelScale = 1.0,
+    uiScale = 1.0
 ) =
   ## Starts Fidget UI library
+  ## 
   common.fullscreen = fullscreen
   common.uiScale = uiScale
   if not fullscreen:
