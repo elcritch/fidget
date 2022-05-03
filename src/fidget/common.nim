f4c158623609f1030a8c34fad31f70bef644c7bc
Jaremy Creechley <creechley@gmail.com>
2022-05-02 23:52:42 -0700

    add alpha transparency

--- src/fidget/common.nim
+++ src/fidget/common.nim
@@ -660,7 +660,13 @@ proc atXY*(rect: Rect, x, y: float64|float32|int): Rect =
   result.x = x.float32
   result.y = y.float32
 
+proc `*`*(color: Color, alpha: float32): Color =
+  ## update alpha on color
+  result = color
+  result.a *= alpha
+
 proc `+`*(rect: Rect, xy: Vec2): Rect =
+  ## offset rect with xy vec2 
   result = rect
   result.x += xy.x
   result.y += xy.y
