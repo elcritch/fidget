8cf53b6d15c4d5456ac7f9bd7d30001e73075c1c
Jaremy Creechley <creechley@gmail.com>
2022-06-18 19:40:14 -0700

    update commons

--- src/fidget/commonutils.nim
+++ src/fidget/commonutils.nim
@@ -43,7 +43,7 @@ type
 borrowMaths(Percent)
 
 type
-  RawVec2* = distinct Vec2
+  RawVec2* = distinct GVec2[float32]
   RawRect* = distinct Rect
 
 macro genOp(T, B: untyped, ops: varargs[untyped]) =
@@ -52,14 +52,25 @@ macro genOp(T, B: untyped, ops: varargs[untyped]) =
     result.add quote do:
       proc `op`*[`T`](a, b: `T`): `T` = `op`(a.`B`, b.`B`).`T`
 
-genOp(RawVec2, Vec2, `+`, `-`, `/`, `*`, `mod`, `div`, `zmod`)
+template genEqOp(T, B: untyped, op: untyped) =
+    template `op`*(a: var RawVec2, b: float32) =
+      `op`(a.`B`, b)
+    proc `op`*(a: var `T`, b: `T`) =
+      `op`(a.`B`, b.`B`)
 
-let x = vec2(12.1, 13.4).RawVec2
-let y = vec2(10.0, 10.0).RawVec2
+genOp(RawVec2, Vec2, `+`, `-`, `/`, `*`, `mod`, `div`, `zmod`)
+genEqOp(RawVec2, Vec2, `+=`)
 
 when isMainModule:
+  let x = vec2(12.1, 13.4).RawVec2
+  let y = vec2(10.0, 10.0).RawVec2
+  var z = vec2(0.0, 0.0).RawVec2
+  z += y
+  z += 3.1'f32
+
   echo "x + y: ", repr(x + y)
   echo "x - y: ", repr(x - y)
   echo "x / y: ", repr(x / y)
   echo "x * y: ", repr(x * y)
+  echo "z: ", repr(z)
 
